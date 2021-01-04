a4dfa4ec19649d9a49c32b0ec7c338cc2535fc04
Arthur Menezes <arthur.menezes@presidencia.gov.br>
2021-01-04 15:57:23 -0300

    Create: Route POST Appointments.

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import routes from './routes';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(process.env.PORT, () => {
+  console.log('Servido rondando na porta: ', process.env.PORT);
+});
